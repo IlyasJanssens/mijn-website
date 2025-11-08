c3b158e40be144dbd2cc7b6c6a931c2bcedac198
Ilyas Janssens <71272789+IlyasJanssens@users.noreply.github.com>
2025-11-08 15:00:43 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToSection(sectionId) {
+            const element = document.getElementById(sectionId);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+        }
+
+        //Stond in project.html onderaan
